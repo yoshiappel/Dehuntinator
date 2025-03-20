b93b5a81dd98e811934fd4abb1760b34d30276df
Piet <piethboterhoek@gmail.com>
2025-03-20 19:14:30 +0100

    Added a second element that needs to be removed

--- Dehuntinator/content.js
+++ Dehuntinator/content.js
@@ -1,14 +1,24 @@
 // Function to remove unwanted elements
 function removeUnwantedElements() {
-    let annoyingElement = document.querySelectorAll(".game-sort-carousel-wrapper");
+    // define which elements you want to remove
+    let annoyingElement0 = document.querySelectorAll(".game-sort-carousel-wrapper");
+    let annoyingElement1 = document.getElementsByClassName("sdui-feed-item-container")[0];
 
-    annoyingElement.forEach(annoyingElement => {
+    // then check if that element exist, if so remove it
+    if (annoyingElement1) {
+        annoyingElement1.remove(); 
+    }
+
+    // if there are multiple elements in the document and you only want to remove a specific one, 
+    // assign the checkElement with a element that only the element that you want to remove has
+    annoyingElement0.forEach(annoyingElement0 => {
         // check if it doesnt contain this code
-        let checkElement = annoyingElement.querySelector(".grid-item-container.game-card-container");
+        let checkElement = annoyingElement0.querySelector(".grid-item-container.game-card-container");
 
-        // if it doesnt contain this item delete it
+        // if it doesnt contain this item remove it
+        // for this case (the hunt on the roblox website) we check if it doesnt contain this element because there are multiple elements with the same name
         if(!checkElement) {
-            annoyingElement.remove();
+            annoyingElement0.remove();
         }
     });
 }
